759c8f96bafdf63a6fc9e1994003cd9d131e68de
Brian Whicheloe <brian@whicheloe.us>
2016-09-20 21:48:44 -0700

    Cleverly style guide

--- store/client-store.js
+++ store/client-store.js
@@ -4,36 +4,33 @@ var io = require('socket.io-client');
 
 var Stream = require('./stream');
 
-
-class Store
-{
-  constructor(config)
-  {
-    this.config = config;
-    this.streams = config.streams;
-    this.basePath = config.basePath ? config.basePath : 'datastore';
-  }
-
-  start(url, port)
-  {
-    this.url = url;
-    this.port = port;
-    this.socket = io.connect(this.url + ':' + this.port, {transports: ['websocket']});
-    this.http = ajax;
-
-    return this.init();
-  }
-
-  init()
-  {
-    var len = this.streams.length;
-
-    for (var i = 0; i < len; i++) {
-      this[this.streams[i]] = new Stream(this.streams[i], this);
+class Store {
+    constructor(config) {
+        this.config = config;
+        this.streams = config.streams;
+        this.basePath = config.basePath ? config.basePath : 'datastore';
     }
 
-    return this;
-  }
+    start(url, port) {
+        this.url = url;
+        this.port = port;
+        this.socket = io.connect(this.url + ':' + this.port, {
+            transports: ['websocket'],
+        });
+        this.http = ajax;
+
+        return this.init();
+    }
+
+    init() {
+        var len = this.streams.length;
+
+        for (var i = 0; i < len; i++) {
+            this[this.streams[i]] = new Stream(this.streams[i], this);
+        }
+
+        return this;
+    }
 }
 
 module.exports = Store;
--- store/client-stream.js
+++ store/client-stream.js
@@ -2,120 +2,112 @@ const crypto = require('crypto');
 const differ = require('jsondiffpatch');
 const io = require('socket.io-client');
 
-class Stream
-{
-  constructor(name)
-  {
-    this.name = name;
-    this.listeners = [];
-    this.socket = io.connect(name, {transports: ['websocket']});
-  }
+class Stream {
+    constructor(name) {
+        this.name = name;
+        this.listeners = [];
+        this.socket = io.connect(name, {
+            transports: ['websocket'],
+        });
+    }
 
-  start(ret)
-  {
-     this.socket.on('connect', () => {
-      this.socket.on('hash', hash => {
-        console.log('initial hash', hash);
+    start(ret) {
+        this.socket.on('connect', () => {
+            this.socket.on('hash', hash => {
+                console.log('initial hash', hash);
 
+                if (localStorage[this.name] === undefined) {
+                    console.log('pre new');
+                    this.retrieve(ret);
+                } else if (JSON.parse(localStorage[this.name]).id !== hash) {
+                    console.log('pre patch');
+                    console.log(JSON.parse(localStorage[this.name]).id, hash);
+                    this.hash = JSON.parse(localStorage[this.name]).id;
+                    this.patch(ret);
+                } else {
+                    console.log('cached');
+                    ret.data = this.send(localStorage[this.name]);
+                }
+            });
+        });
 
-        if (localStorage[this.name] === undefined) {
-          console.log('pre new');
-          this.retrieve(ret);
-        } else if (JSON.parse(localStorage[this.name]).id !== hash) {
-          console.log('pre patch');
-          console.log(JSON.parse(localStorage[this.name]).id, hash);
-          this.hash = JSON.parse(localStorage[this.name]).id;
-          this.patch(ret);
-        } else {
-          console.log('cached');
-          ret.data = this.send(localStorage[this.name]);
-        }
-      });
-    });
+        return this;
+    }
 
-    return this;
-  }
+    grab(cb) {
+        this.listeners.push(cb.bind(this));
+    }
 
-  grab(cb)
-  {
-    this.listeners.push(cb.bind(this));
-  }
+    send(data) {
+        return JSON.parse(JSON.parse(data).data);
+    }
 
-  send(data)
-  {
-    return JSON.parse(JSON.parse(data).data);
-  }
+    // send(data)
+    // {
+    //   let listeners = this.listeners;
+    //   data = JSON.parse(JSON.parse(data).data);
+    //
+    //   if (listeners.length > 0) {
+    //     if (listeners.length > 1) {
+    //       listeners.forEach(el => {
+    //         el(data);
+    //       });
+    //     } else {
+    //       listeners[0](data);
+    //     }
+    //   }
+    // }
 
-  // send(data)
-  // {
-  //   let listeners = this.listeners;
-  //   data = JSON.parse(JSON.parse(data).data);
-  //
-  //   if (listeners.length > 0) {
-  //     if (listeners.length > 1) {
-  //       listeners.forEach(el => {
-  //         el(data);
-  //       });
-  //     } else {
-  //       listeners[0](data);
-  //     }
-  //   }
-  // }
+    retrieve(ret) {
+        this.socket.emit('full', null, data => {
+            console.log(data);
+            let hash = crypto.createHash('md5').update(data).digest('hex');
+            console.log('new');
 
-  retrieve(ret)
-  {
-    this.socket.emit('full', null, data => {
-      console.log(data);
-      let hash = crypto.createHash('md5').update(data).digest("hex");
-      console.log('new');
+            localStorage[this.name] = JSON.stringify({
+                id: hash,
+                data: data
+            });
 
-      localStorage[this.name] = JSON.stringify({
-        id: hash,
-        data: data
-      });
+            ret.data = this.send(localStorage[this.name]);
+        });
+    }
 
+    patch(ret) {
+        this.socket.emit('patch', this.hash, data => {
+            console.log(data);
 
-      ret.data = this.send(localStorage[this.name]);
-    });
-  }
+            if (Array.isArray(data)) {
+                console.log('patch');
+                data = differ.patch(JSON.parse(localStorage[this.name]).data, data);
+                console.log(data);
+            } else {
+                console.log('tried patch. need full.');
+            }
 
-  patch(ret)
-  {
-    this.socket.emit('patch', this.hash, data => {
-      console.log(data);
+            let hash = crypto.createHash('md5').update(data).digest('hex');
 
-      if (Array.isArray(data)) {
-        console.log('patch');
-        data = differ.patch(JSON.parse(localStorage[this.name]).data, data);
-        console.log(data);
-      } else {
-        console.log('tried patch. need full.');
-      }
+            localStorage[this.name] = JSON.stringify({
+                id: hash,
+                data: data,
+            });
 
-      let hash = crypto.createHash('md5').update(data).digest("hex");
+            ret.data = this.send(localStorage[this.name]);
+        });
+    }
 
-      localStorage[this.name] = JSON.stringify({
-        id: hash,
-        data: data
-      });
+    call(action, props, cb) {
+        let key = this.name + '.' + action;
+        console.log('calling ' + action);
+        props = props ? props : [];
+        this.socket.emit(key, props, data => {
+            cb(data);
+        });
 
-      ret.data = this.send(localStorage[this.name]);
-    });
-  }
-
-  call(action, props, cb)
-  {
-    let key = this.name + '.' + action;
-    console.log('calling ' + action);
-    props = props ? props : [];
-    this.socket.emit(key, props, data => {
-      cb(data);
-    });
-
-    // this.socket.on(key, data => {
-    //   cb(data);
-    // });
-  }
+        // this.socket.on(key, data => {
+        //   cb(data);
+        // });
+    }
 }
 
 module.exports = Stream;
