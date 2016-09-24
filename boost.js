2b3b35fd337c8566acf8a45613e9a27f83a90fdd
Brian Whicheloe <brian@whicheloe.us>
2016-09-23 18:26:36 -0700

    Add vue to boost-client

--- boost.js
+++ boost.js
@@ -1,16 +1,39 @@
+const Vue = require('vue');
+const Router = require('vue-router');
 const io = require('socket.io-client');
 const hash = require('object-hash');
 const path = require('path');
 const Stream = require('./store/client-stream');
-// import Stream from './store/client-stream.js';
+// const resource = require('vue-resource');
+
+Vue.use(Router);
 
 class Boost {
+    constructor() {
+        this.vue = Vue;
+        this.socket = io.connect('/application', {transports: ['websocket']});
+        this.registerListeners();
+    }
 
-	constructor() {
-		this.socket = io.connect('/application', {transports: ['websocket']});
+    start(routes) {
+        let router = new Router({
+            mode: 'history',
+            history: true,
+            routes,
+        });
 
-		this.registerListeners();
-	}
+        new Vue({
+            router,
+        }).$mount('#app');
+
+        this.router.beforeEach(function(transition) {
+            window.scrollTo(0, 0);
+            // if (!transition.to.matched) {
+            //     transition.redirect('/404');
+            // }
+            transition.next();
+        });
+    }
 
     subscribe(path) {
         let ret = {};
@@ -22,24 +45,27 @@ class Boost {
     }
 
     registerListeners() {
-    	console.log('listening');
+        console.log('listening');
 
-    	// Any WebSocket listeners here...
-    	
-    	this.socket.on('connect', socket => {
-    		console.log('app:ready!');
-    	})
+        // Any WebSocket listeners here...
+
+        this.socket.on('connect', socket => {
+            console.log('app:ready!');
+        });
+    }
+
+    login() {
 
     }
 
     logout() {
-    	let token = localStorage.token;
-    	console.log('auth:logging-out');
+        let token = localStorage.token;
+        console.log('auth:logging-out');
 
-    	this.socket.emit('auth.logout', token, () => {
-    		console.log('auth:logged-out')
-    		localStorage.removeItem('token');
-    	});
+        this.socket.emit('auth.logout', token, () => {
+            console.log('auth:logged-out');
+            localStorage.removeItem('token');
+        });
     }
 
 }
